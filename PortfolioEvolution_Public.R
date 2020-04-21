13a630fb176a9251e6bacf4a9f36943c171cb858
datruccone <37838258+datruccone@users.noreply.github.com>
2020-04-21 13:58:48 +0200

    Update PortfolioEvolution_Public.R
    Feed change, now uses https://eodhistoricaldata.com/
    You have to register on that webpage to get your personal API ID to use it on R script

--- PortfolioEvolution_Public.R
+++ PortfolioEvolution_Public.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(data.table)
 #library("xlsx")
 library(dplyr)
-library(BatchGetSymbols)
+#library(BatchGetSymbols)
 library("ggplot2")
 library(grDevices)
 library(hrbrthemes) #Themes for ggplot2
@@ -23,7 +23,7 @@ library(writexl)
 #Cashflow: keeps records of deposits and withdrawals from/to your Investment account/s. Each movement must be added manually to this data frame
 
 
-setwd('c:/Path/to/your/Workspace/')
+setwd('c:/Pathtoyourfolder/')
 #load("Portfolio Evolution.RData")
 
 commarep <- function(c) {sub(",",".",c)}
@@ -61,6 +61,10 @@ degiro_csv_link <- readClipboard()
 degiro_csv_toDate <- as.integer(gregexpr("toDate" ,degiro_csv_link))
 degiro_csv_date <- as.Date(paste0(substr(degiro_csv_link,degiro_csv_toDate + 17, degiro_csv_toDate + 21),"-",substr(degiro_csv_link,degiro_csv_toDate + 12, degiro_csv_toDate + 13),"-",substr(degiro_csv_link,degiro_csv_toDate + 7, degiro_csv_toDate + 8)))
 
+#Open Local file with Portfolio's daily movements
+Portfolio_Daily <- read_excel("Portfolio_Daily.xlsx")
+Portfolio_Daily$Date <- as.Date(Portfolio_Daily$Date,format = "%Y-%m-%d")
+
 #Check if Degiro CSV's hasn't been already imported to our Portfolio Daily dataframe...
 if (degiro_csv_date %!in% Portfolio_Daily$Date) 
 {
@@ -70,8 +74,6 @@ if (degiro_csv_date %!in% Portfolio_Daily$Date)
   CSV_Degiro <- CSV_Degiro[,c(7,1:6)]
   names(CSV_Degiro) <- c("Date","Product","ISIN","Amount","Closing","Localvalue","ValueinEUR")
   #Append to Portfolio Daily history
-  Portfolio_Daily <- read_excel("Portfolio_Daily.xlsx")
-  Portfolio_Daily$Date <- as.Date(Portfolio_Daily$Date,format = "%Y-%m-%d")
   Portfolio_Daily <- rbind(Portfolio_Daily,CSV_Degiro,use.names=FALSE)
   Portfolio_Daily <- Portfolio_Daily[order(-Portfolio_Daily$Date),]
   #names(Portfolio_Daily) <- c("Date","Product","ISIN","Amount","Closing","Localvalue","ValueinEUR")
@@ -102,48 +104,56 @@ Portfolio_Evolution$Earnings_Acc <- Portfolio_Evolution$Portfolio - Portfolio_Ev
 Portfolio_Evolution$HPR[1] <- 0 
 Portfolio_Evolution$HPR_Acc <- cumsum(Portfolio_Evolution$HPR)
 
-#Indexes Import, this piece of code is based on: https://cran.r-project.org/web/packages/BatchGetSymbols/vignettes/BatchGetSymbols-vignette.html
-l.out <- BatchGetSymbols(tickers = c('WLD.PA','IUES.AS','^STOXX50E','XQUI.MI','TOF.AS','F703.DE'), 
-                         first.date = '2018-7-24',   #You can use another date
-                         last.date = Sys.Date(), 
-                         freq.data = 'daily',
-                         cache.folder = file.path(tempdir(), 
-                                                  'BGS_Cache') ) # cache in tempdir()
+# New Feed data connection (https://eodhistoricaldata.com/)  # Control Panel: https://eodhistoricaldata.com/cp/settings
+EOD_HD_API <- "YOUR API ID"
+tickers <- c('IWDA.AS','XD9U.XETRA','STOXX.INDX','XQUI.MI','TOF.AS','F703.XETRA')
+first_date = as.Date('2018-01-02')   #You can use another date
+last_date = Sys.Date() - 1
 
-#Filtering and cleaning Markets data:
-Indexes <- l.out$df.tickers
-WORLD_Dev <- data.frame(subset(l.out$df.tickers[c(7,8,6)],l.out$df.tickers$ticker == "WLD.PA"))      #MSCI World Index ETF in EUR (because the Portfolio is EUR, so I'm matching same currencies)
-WORLD_Dev[,4] <- ((WORLD_Dev$price.adjusted/shift(WORLD_Dev$price.adjusted))-1) 
-SP500 <- data.frame(subset(l.out$df.tickers[c(7,8,6)],l.out$df.tickers$ticker == "IUES.AS"))         #SP500 ETF in EUR (because the Portfolio is EUR, so I'm matching same currencies)
-SP500[,4] <- ((SP500$price.adjusted/shift(SP500$price.adjusted))-1) 
-STOXX50 <- data.frame(subset(l.out$df.tickers[c(7,8,6)],l.out$df.tickers$ticker == "^STOXX50E"))     #STOXX50 Index
-STOXX50[,4] <- ((STOXX50$price.adjusted/shift(STOXX50$price.adjusted))-1) 
-Xtrackers <- data.frame(subset(l.out$df.tickers[c(7,8,6)],l.out$df.tickers$ticker == "XQUI.MI"))     #The Portfolio index tracks a globally diversified portfolio consisting of equities and bond indices. The tactical allocation may change up to 8 times per year. Equity share: minimum 30%, maximum 70%. Bond share: minimum 30%, maximum 70%.
-Xtrackers[,4] <- ((Xtrackers$price.adjusted/shift(Xtrackers$price.adjusted))-1) 
-VanEck_Off <- data.frame(subset(l.out$df.tickers[c(7,8,6)],l.out$df.tickers$ticker == "TOF.AS"))     #The aim of the VanEck Vectors™ Multi-Asset Growth Allocation UCITS ETF is to follow the Multi-Asset Growth Allocation Index as closely as possible. This is a composite index made up in the ratios indicated here: - 60% Solactive Global Equity Index - 10% GPR Global 100 Index - 15% Markit iBoxx EUR Liquid Corporates Index - 15% Markit iBoxx EUR Liquid Sovereign Diversified 1-10 Index
-VanEck_Off[,4] <- ((VanEck_Off$price.adjusted/shift(VanEck_Off$price.adjusted))-1) 
-ComStage_Off <- data.frame(subset(l.out$df.tickers[c(7,8,6)],l.out$df.tickers$ticker == "F703.DE"))  #The ComStage Vermögensstrategie Offensiv index tracks a diversified ETF portfolio. The initial allocation is made up of the following asset classes: 80% of global equities spread across geographies and across sectors, 10% of bonds and 10% commodities. Annually, the index is rebalanced.
-ComStage_Off[,4] <- ((ComStage_Off$price.adjusted/shift(ComStage_Off$price.adjusted))-1) 
+# Links to EOD API (One ticker download at the time, maximum 20 per day)
+EOD_link <- vector()
+for (a in 1:length(tickers)){
+  EOD_link[a] <- paste0('https://eodhistoricaldata.com/api/eod/',tickers[a],'?from=',first_date,'&to=',last_date,'&api_token=',EOD_HD_API,'&period=d&order=a')
+}
+rm(a)
+
+# Function to download and adapt new feed to old feed
+Adapt_data <- function(df,position){
+  df <- data.frame(fread(EOD_link[position]))[,c(1,6)]
+  df <- data.frame(cbind(df,tickers[position]))
+  df <- df[,c(1,3,2)]
+  df <- df[!df$Adjusted_close == 0,]
+  df$Adjusted_close <- as.numeric(df$Adjusted_close) 
+  df[,4] <- ((df$Adjusted_close/shift(df$Adjusted_close))-1) 
+  df$Date <- as.Date(df$Date) 
+  return(df)
+}
+
+WORLD_Dev <- Adapt_data(WORLD_Dev,1) #MSCI World Index ETF
+USA <- Adapt_data(USA,2)              #USA ETF
+STOXX600 <- Adapt_data(STOXX600,3)     #STOXX600 Index
+Xtrackers <- Adapt_data(Xtrackers,4)     #The Portfolio index tracks a globally diversified portfolio consisting of equities and bond indices. The tactical allocation may change up to 8 times per year. Equity share: minimum 30%, maximum 70%. Bond share: minimum 30%, maximum 70%.
+VanEck_Off <- Adapt_data(VanEck_Off,5)     #The aim of the VanEck  Multi-Asset Growth Allocation UCITS ETF is to follow the Multi-Asset Growth Allocation Index as closely as possible. This is a composite index made up in the ratios indicated here: - 60% Solactive Global Equity Index - 10% GPR Global 100 Index - 15% Markit iBoxx EUR Liquid Corporates Index - 15% Markit iBoxx EUR Liquid Sovereign Diversified 1-10 Index
+ComStage_Off <- Adapt_data(ComStage_Off,6)  #The ComStage  Offensiv index tracks a diversified ETF portfolio. The initial allocation is made up of the following asset classes: 80% of global equities spread across geographies and across sectors, 10% of bonds and 10% commodities. Annually, the index is rebalanced.
 
 names(WORLD_Dev) <- c("Date","Ticker","WORLD_Dev_Price","WORLD_Dev_Change")
-names(SP500) <- c("Date","Ticker","SP500_Price","SP500_Change")
-names(STOXX50) <- c("Date","Ticker","STOXX50_Price","STOXX50_Change")
+names(USA) <- c("Date","Ticker","USA_Price","USA_Change")
+names(STOXX600) <- c("Date","Ticker","STOXX600_Price","STOXX600_Change")
 names(Xtrackers) <- c("Date","Ticker","Xtrackers_Price","Xtrackers_Change")
 names(VanEck_Off) <- c("Date","Ticker","VanEck_Off_Price","VanEck_Off_Change")
 names(ComStage_Off) <- c("Date","Ticker","ComStage_Off_Price","ComStage_Off_Change")
 
-Portfolio_Evolution <- Reduce(function(x, y) merge(x, y, by="Date", all=TRUE), list(Portfolio_Evolution,WORLD_Dev,SP500,STOXX50,Xtrackers,VanEck_Off,ComStage_Off))
+Portfolio_Evolution <- Reduce(function(x, y) merge(x, y, by="Date", all=TRUE), list(Portfolio_Evolution,WORLD_Dev,USA,STOXX600,Xtrackers,VanEck_Off,ComStage_Off))
 
-rm(degiro_csv_link,degiro_csv_toDate,l.out,WORLD_Dev,Xtrackers,SP500,STOXX50,VanEck_Off,ComStage_Off) #Delete Data Frames that I'm not going to use anymore
+rm(degiro_csv_link,degiro_csv_toDate,l.out,WORLD_Dev,Xtrackers,USA,STOXX600,VanEck_Off,ComStage_Off) #Delete Data Frames that I'm not going to use anymore
 
 #Last makeup...
 Portfolio_Evolution <- Portfolio_Evolution[c(1:7,10,13,16,19,22,25,9,12,15,18,21,24)]
 
-
 Portfolio_Evolution <- cbind(Portfolio_Evolution[c(1:7)],round(Portfolio_Evolution[,8:13]*100,2),round(Portfolio_Evolution[,14:19],2))
 Portfolio_Evolution <- Portfolio_Evolution[!chron::is.weekend(as.Date(Portfolio_Evolution$Date, "%d/%m/%Y")), ]
-names(Portfolio_Evolution)[8:13] <- c("WORLD_Dev_Change","SP500_Change","STOXX50_Change","Xtrackers_Change","VanEck_Off_Change","ComStage_Off_Change")
-names(Portfolio_Evolution)[14:19] <- c("WORLD_Dev_Price","SP500_Price","STOXX50_Price","Xtrackers_Price","VanEck_Off_Price","ComStage_Off_Price")
+names(Portfolio_Evolution)[8:13] <- c("WORLD_Dev_Change","USA_Change","STOXX600_Change","Xtrackers_Change","VanEck_Off_Change","ComStage_Off_Change")
+names(Portfolio_Evolution)[14:19] <- c("WORLD_Dev_Price","USA_Price","STOXX600_Price","Xtrackers_Price","VanEck_Off_Price","ComStage_Off_Price")
 
 #Plotting:
 last_degiro_deposit <- as.Date(Cashflow_Degiro[nrow(Cashflow_Degiro),1])
@@ -162,8 +172,8 @@ names(Evo_Comp_return)[1] <- "Date"
 Cum_Plot <- ggplot(Evo_Comp_return,aes(Date)) +
   geom_line(aes(y = Portfolio, colour = "0_My Portfolio"),size=1.5) + 
   geom_line(aes(y = WORLD_Dev_Price, colour = "WORLD_Dev"),size=0.4) +         
-  geom_line(aes(y = SP500_Price, colour = "SP500"),size=0.4) + 
-  geom_line(aes(y = STOXX50_Price, colour = "STOXX50"),size=0.4) +
+  geom_line(aes(y = USA_Price, colour = "USA"),size=0.4) + 
+  geom_line(aes(y = STOXX600_Price, colour = "STOXX600"),size=0.4) +
   geom_line(aes(y = Xtrackers_Price, colour = "Xtrackers"),size=0.4) + 
   geom_line(aes(y = VanEck_Off_Price, colour = "VanEck_Off"),size=0.4) +
   geom_line(aes(y = ComStage_Off_Price, colour = "ComStage_Off"),size=0.4) +    
@@ -173,13 +183,12 @@ Cum_Plot <- ggplot(Evo_Comp_return,aes(Date)) +
   labs(colour = "Portfolios and Indexes") +
   xlim(as.Date(c(last_degiro_deposit, Sys.Date()), format="%d/%m/%Y"))
 #        ylim(-10,10))
-####
 
 Daily_Plot <- ggplot(Portfolio_Evolution,aes(Date)) +
   geom_line(aes(y = HPR, colour = "0_My Portfolio"),size=1.5) + 
   geom_line(aes(y = WORLD_Dev_Change, colour = "WORLD_Dev"),size=0.4) + 
-  geom_line(aes(y = SP500_Change, colour = "SP500"),size=0.4) + 
-  geom_line(aes(y = STOXX50_Change, colour = "STOXX50"),size=0.4) +
+  geom_line(aes(y = USA_Change, colour = "USA"),size=0.4) + 
+  geom_line(aes(y = STOXX600_Change, colour = "STOXX600"),size=0.4) +
   geom_line(aes(y = Xtrackers_Change, colour = "Xtrackers"),size=0.4) + 
   geom_line(aes(y = VanEck_Off_Change, colour = "VanEck_Off"),size=0.4) +
   geom_line(aes(y = ComStage_Off_Change, colour = "ComStage_Off"),size=0.4) +    
@@ -197,7 +206,7 @@ multiplot(Cum_Plot,Daily_Plot)
 #Preparing Portfolio vs Benchmarks evolution report to present on the Console:
 Days <- list(7,14,30,45,90,180,365) #Different time frames to show on the report
 rm(Portfolio_vs_Benchmarks,Portfolio_vs_Benchmarks_Temp)
-Portfolio_vs_Benchmarks <- data.frame(Days = as.character(), Portfolio = as.numeric(), WORLD_Dev_Change = as.numeric(), SP500_Change = as.numeric(), STOXX50_Change = as.numeric(), Xtrackers_Change = as.numeric(), VanEck_Off_Change = as.numeric(), ComStage_Off_Change = as.numeric())
+Portfolio_vs_Benchmarks <- data.frame(Days = as.character(), Portfolio = as.numeric(), WORLD_Dev_Change = as.numeric(), USA_Change = as.numeric(), STOXX600_Change = as.numeric(), Xtrackers_Change = as.numeric(), VanEck_Off_Change = as.numeric(), ComStage_Off_Change = as.numeric())
 
 #Last Day:
 Portfolio_vs_Benchmarks_Temp <- data.frame(Portfolio=as.numeric()) #Temporary Data Frame
@@ -210,13 +219,13 @@ if (!is.na(Portfolio_Evolution[nrow(Portfolio_Evolution)-1,c(8)])) {
 } else {
   Portfolio_vs_Benchmarks_Temp$WORLD_Dev_Change <- 'S/D'}
 if (!is.na(Portfolio_Evolution[nrow(Portfolio_Evolution)-1,c(9)])) {
-  Portfolio_vs_Benchmarks_Temp$SP500_Change <- Portfolio_Evolution[nrow(Portfolio_Evolution),c(9)]
+  Portfolio_vs_Benchmarks_Temp$USA_Change <- Portfolio_Evolution[nrow(Portfolio_Evolution),c(9)]
 } else {
-  Portfolio_vs_Benchmarks_Temp$SP500_Change <- 'S/D'}
+  Portfolio_vs_Benchmarks_Temp$USA_Change <- 'S/D'}
 if (!is.na(Portfolio_Evolution[nrow(Portfolio_Evolution)-1,c(10)])) {
-  Portfolio_vs_Benchmarks_Temp$STOXX50_Change <- Portfolio_Evolution[nrow(Portfolio_Evolution),c(10)]
+  Portfolio_vs_Benchmarks_Temp$STOXX600_Change <- Portfolio_Evolution[nrow(Portfolio_Evolution),c(10)]
 } else {
-  Portfolio_vs_Benchmarks_Temp$STOXX50_Change <- 'S/D'}
+  Portfolio_vs_Benchmarks_Temp$STOXX600_Change <- 'S/D'}
 if (!is.na(Portfolio_Evolution[nrow(Portfolio_Evolution)-1,c(11)])) {
   Portfolio_vs_Benchmarks_Temp$Xtrackers_Change <- Portfolio_Evolution[nrow(Portfolio_Evolution),c(11)]
 } else {
@@ -266,7 +275,7 @@ Portfolio_vs_Benchmarks_Temp <- Portfolio_vs_Benchmarks_Temp[,c(8,1:7)]
 Portfolio_vs_Benchmarks_Temp <- Portfolio_vs_Benchmarks_Temp %>% mutate_at(vars(-Days), funs(round(., 2)))
 Portfolio_vs_Benchmarks <- data.frame(rbind(Portfolio_vs_Benchmarks,Portfolio_vs_Benchmarks_Temp,use.names=FALSE))
 Portfolio_vs_Benchmarks <- Portfolio_vs_Benchmarks[-nrow(Portfolio_vs_Benchmarks),]
-names(Portfolio_vs_Benchmarks) <- c("Days","Portfolio","WORLD_Dev","SP500","STOXX50","Xtrackers","VanEck_Off","ComStage_Off")
+names(Portfolio_vs_Benchmarks) <- c("Days","Portfolio","WORLD_Dev","USA","STOXX600","Xtrackers","VanEck_Off","ComStage_Off")
 rm(Days,i,Portfolio_vs_Benchmarks_Temp,Evo_Comp,Evo_Comp_filter)
 #cat("\f")
 cat (paste0("Evolution during last n days: "))
