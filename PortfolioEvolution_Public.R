8319f13adcac7ded974d16b5a06811497c617721
datruccone <37838258+datruccone@users.noreply.github.com>
2020-04-22 12:06:28 +0200

    New Feature
    The script will automatically show you at the end your Account Statement from last 7 Days. So you will know if you got some dividends, MMF movements, etc.
    Also renamed some variables used for csv importing

--- PortfolioEvolution_Public.R
+++ PortfolioEvolution_Public.R
@@ -22,7 +22,7 @@ library(writexl)
 #Portfolio_vs_Benchmarks: Portfolio vs Benchmarks on different time frames
 #Cashflow: keeps records of deposits and withdrawals from/to your Investment account/s. Each movement must be added manually to this data frame
 
-
+rm(list = ls(all.names = TRUE))
 setwd('c:/Pathtoyourfolder/')
 #load("Portfolio Evolution.RData")
 
@@ -38,18 +38,18 @@ cat("\f")
 
 #degiro_csv_link <- readline(prompt="Paste Degiro's CSV link: ")  
 #degiro_csv_fromDate <- readline(prompt="Enter From Date in YYYY-MM-DD Format: ") 
-#degiro_csv_toDate <- readline(prompt="Enter To Date in YYYY-MM-DD Format: ") 
-#days = as.integer(as.Date(degiro_csv_toDate) - as.Date(degiro_csv_fromDate))+1
+#degiro_csv_toDate_pos <- readline(prompt="Enter To Date in YYYY-MM-DD Format: ") 
+#days = as.integer(as.Date(degiro_csv_toDate_pos) - as.Date(degiro_csv_fromDate))+1
 #Portfolio_Daily <- data.frame(Date = as.character(), Product = as.character(), ISIN = as.character(), Amount = as.numeric(), Closing = as.numeric(), Localvalue = as.character(), ValueinEUR = as.character())
-#degiro_csv_date <- degiro_csv_fromDate
+#degiro_csv_toDate <- degiro_csv_fromDate
 #for (i in 1:days){ 
-#  degiro_csv_link <- paste0(substr(degiro_csv_link,1, nchar(degiro_csv_link)-14),substr(degiro_csv_date,9, 10),"%2F",substr(degiro_csv_date,6, 7),"%2F",substr(degiro_csv_date,1, 4))
+#  degiro_csv_link <- paste0(substr(degiro_csv_link,1, nchar(degiro_csv_link)-14),substr(degiro_csv_toDate,9, 10),"%2F",substr(degiro_csv_toDate,6, 7),"%2F",substr(degiro_csv_toDate,1, 4))
 #  CSV_Degiro <- fread(degiro_csv_link)
-#  CSV_Degiro$Date <- degiro_csv_date
+#  CSV_Degiro$Date <- degiro_csv_toDate
 #  CSV_Degiro <- CSV_Degiro[,c(7,1:6)]
 #  #Append to Portfolio Daily history
 #  Portfolio_Daily <- rbind(Portfolio_Daily,CSV_Degiro,use.names=FALSE)
-#  degiro_csv_date <- as.character(as.Date(degiro_csv_date) %m+% days(1))
+#  degiro_csv_toDate <- as.character(as.Date(degiro_csv_toDate) %m+% days(1))
 #  #Delete CSV Data Frame
 #  rm(CSV_Degiro)
 #}
@@ -58,19 +58,32 @@ cat("\f")
 
 # One Day import:
 degiro_csv_link <- readClipboard()
-degiro_csv_toDate <- as.integer(gregexpr("toDate" ,degiro_csv_link))
-degiro_csv_date <- as.Date(paste0(substr(degiro_csv_link,degiro_csv_toDate + 17, degiro_csv_toDate + 21),"-",substr(degiro_csv_link,degiro_csv_toDate + 12, degiro_csv_toDate + 13),"-",substr(degiro_csv_link,degiro_csv_toDate + 7, degiro_csv_toDate + 8)))
+degiro_csv_toDate_pos <- as.integer(gregexpr("toDate" ,degiro_csv_link))
+degiro_csv_toDate <- as.Date(paste0(substr(degiro_csv_link,degiro_csv_toDate_pos + 17, degiro_csv_toDate_pos + 21),"-",substr(degiro_csv_link,degiro_csv_toDate_pos + 12, degiro_csv_toDate_pos + 13),"-",substr(degiro_csv_link,degiro_csv_toDate_pos + 7, degiro_csv_toDate_pos + 8)))
+
+# Gererate link to Account Statment Import
+degiro_csv_fromDate <- degiro_csv_toDate - 8
+degiro_acc_stat_link <- sub('positionReport','cashAccountReport',degiro_csv_link)
+degiro_acc_stat_link <- sub('&country=IE&lang=en',paste0('&country=IE&lang=en&fromDate=',substr(degiro_csv_fromDate,9,10),'%2F',substr(degiro_csv_fromDate,6,7),'%2F',substr(degiro_csv_fromDate,1,4)),degiro_acc_stat_link)
+
+# Download Account Statment CSV
+Degiro_acc_stat <- fread(degiro_acc_stat_link)
+
+# Cleaning Acc Stat
+Degiro_acc_stat <- Degiro_acc_stat[,c(1,4:11)]
+names(Degiro_acc_stat)[6:9] <- c("Change Cur.", "Change", "Balance Cur.", "Balance")
+rm(degiro_acc_stat_link,degiro_csv_fromDate)
 
 #Open Local file with Portfolio's daily movements
 Portfolio_Daily <- read_excel("Portfolio_Daily.xlsx")
 Portfolio_Daily$Date <- as.Date(Portfolio_Daily$Date,format = "%Y-%m-%d")
 
 #Check if Degiro CSV's hasn't been already imported to our Portfolio Daily dataframe...
-if (degiro_csv_date %!in% Portfolio_Daily$Date) 
+if (degiro_csv_toDate %!in% Portfolio_Daily$Date) 
 {
   #...if not, start with CSV file import 
   CSV_Degiro <- fread(degiro_csv_link)
-  CSV_Degiro$Date <- degiro_csv_date
+  CSV_Degiro$Date <- degiro_csv_toDate
   CSV_Degiro <- CSV_Degiro[,c(7,1:6)]
   names(CSV_Degiro) <- c("Date","Product","ISIN","Amount","Closing","Localvalue","ValueinEUR")
   #Append to Portfolio Daily history
@@ -199,7 +212,7 @@ Daily_Plot <- ggplot(Portfolio_Evolution,aes(Date)) +
   xlim(as.Date(c(plot_startdate, Sys.Date()), format="%d/%m/%Y")) 
 #  ylim(-5,5)
 
-#dev.new()
+dev.new()
 multiplot(Cum_Plot,Daily_Plot)
 
 
@@ -278,9 +291,11 @@ Portfolio_vs_Benchmarks <- Portfolio_vs_Benchmarks[-nrow(Portfolio_vs_Benchmarks
 names(Portfolio_vs_Benchmarks) <- c("Days","Portfolio","WORLD_Dev","USA","STOXX600","Xtrackers","VanEck_Off","ComStage_Off")
 rm(Days,i,Portfolio_vs_Benchmarks_Temp,Evo_Comp,Evo_Comp_filter)
 #cat("\f")
-cat (paste0("Evolution during last n days: "))
-print.data.frame(Portfolio_vs_Benchmarks,quote=FALSE,row.names = FALSE)
-#view(Portfolio_vs_Benchmarks,"Evolution")
+#cat (paste0("Evolution during last n days: "))
+#print.data.frame(Portfolio_vs_Benchmarks,quote=FALSE,row.names = FALSE)
+#print.data.frame(Degiro_acc_stat,quote=FALSE,row.names = FALSE)
+view(Portfolio_vs_Benchmarks,title = "Evolution")
+view(Degiro_acc_stat, title = 'Account Statement - One Week')
 
 save.image("Portfolio Evolution.RData")
 
